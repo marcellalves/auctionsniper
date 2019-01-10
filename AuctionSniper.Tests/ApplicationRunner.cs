fbee73452753f319f6311bad3be84bf5cd435e42
Marcell Nascimento Alves <marcell.alves@squadra.com.br>
2019-01-10 08:38:43 -0200

    Alterando o nome do projeto de testes.

---
+++ AuctionSniper.Tests/ApplicationRunner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuctionSniper.Tests
+{
+    internal class ApplicationRunner
+    {
+        internal Task StartBiddingIn(FakeAuctionServer auction)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task ShowsSniperHasLostAuction()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AuctionSniper.Tests/FakeAuctionServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AuctionSniper.Tests
+{
+    internal class FakeAuctionServer
+    {
+        private string v;
+
+        public FakeAuctionServer(string v)
+        {
+            this.v = v;
+        }
+
+        internal Task StartSellingItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task HasReceivedJoinRequestFromSniper()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Task AnnounceClosed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
