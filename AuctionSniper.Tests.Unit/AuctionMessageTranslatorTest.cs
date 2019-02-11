b0face41eba1304e48cf2184ef61072d9170447d
Marcell Nascimento Alves <marcell.alves@gmail.com>
2019-02-11 13:33:42 -0200

    Incluindo o primeiro teste unitário.

---
+++ AuctionSniper.Tests.Unit/AuctionMessageTranslatorTest.cs
@@ -0,0 +1,34 @@
+﻿using AuctionSniper.Domain;
+using AuctionSniper.Fakes.XmppServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+
+namespace AuctionSniper.Tests.Unit
+{
+    [TestClass]
+    public class AuctionMessageTranslatorTest
+    {
+        public readonly Chat _unusedChat = null;
+        private string _sniperId = "test";
+        private MockRepository _mocks;
+        private IAuctionEventListener _mockListener;
+        private AuctionMessageTranslator _translator;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mocks = new MockRepository();
+            _mockListener = _mocks.StrictMock<IAuctionEventListener>();
+            _translator = new AuctionMessageTranslator(_sniperId, _mockListener);
+        }
+
+        [TestMethod]
+        public void NotifiesAuctionClosedWhenCloseMessageReceived()
+        {
+            var message = new Message(_unusedChat) {Body = "SOLVersion: 1.1; Event: CLOSE;"};
+            var mlea = new MessageListenerEventArgs(message);
+
+            _translator.InvokeProcessMessage(mlea);
+        }
+    }
+}
---
+++ AuctionSniper.Tests.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AuctionSniper.Tests.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AuctionSniper.Tests.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d7d9a379-790f-4091-ac6e-17d236bda499")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
